e4df141562ce9c183c2939b7506a0d88b629f9ba
Guilherme Andrade <guilherme_gsa@live.com>
2024-10-14 23:08:06 -0300

    Keep user logged in

--- src/helpers/session.ts
+++ src/helpers/session.ts
@@ -1,14 +1,33 @@
 import { cookies } from "next/headers";
-import { decrypt } from "./jwt";
+import { decrypt, encrypt } from "./jwt";
+
+const generateExpires = () => new Date(Date.now() + 60 * 1000);
+const SESSION_NAME = "session";
 
 export const createSession = (jwt: string) => {
-    const expires = new Date(Date.now() + 60 * 1000);
-    cookies().set("session", jwt, { expires, httpOnly: true });
+    const expires = generateExpires();
+    cookies().set(SESSION_NAME, jwt, { expires, httpOnly: true });
 }
 
 export const getSession = async () => {
-    const session = cookies().get("session")?.value;
+    const session = cookies().get(SESSION_NAME)?.value;
     if (!session) return null;
 
     return await decrypt(session);
+}
+
+export const updateSession = async () => {
+    const session = await getSession();
+    if (!session) return null;
+    const expires = generateExpires();
+    const jwt = await encrypt({ ...session, expires });
+
+    const updateSession = {
+        name: SESSION_NAME,
+        value: jwt,
+        expires,
+        httpOnly: true
+    };
+
+    return updateSession;
 }
---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { updateSession } from "./helpers/session";
+
+export async function middleware(req: NextRequest) {
+    const updatedSession = await updateSession();
+    if (!updatedSession) return null;
+
+    const res = NextResponse.next();
+    res.cookies.set(updatedSession);
+    return res;
+}
